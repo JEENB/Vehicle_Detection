a5fb00374310af48f250a27184f0118a3452ace0
JEENB <bjenish12@gmail.com>
2021-12-12 01:12:59 +0545

    config setup

--- utils.py
+++ utils.py
@@ -83,3 +83,44 @@ def feature_set_viz(feature_set,title, columns = 4, rows = 10, figsize = (15,15)
 
 def save_as_pickle(dic, file_path):
 	pickle.dump(dic, open(file_path, 'wb') )
+
+
+# def draw_boxes(image, block_boxes):
+# 	for box in block_boxes:
+# 		cv2.rectangle(image, box[0], box[1], color=(0,255,0), thick = 6)
+
+# def heat(heatmap, box_list):
+# 	'''
+# 	iterates through each boxes assuming each box is ((x1,y1), (x2,y2))
+# 	'''
+# 	for box in box_list:
+# 		heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+# 	return heatmap
+
+
+# def apply_threshold(heatmap, threshold):
+#     # Zero out pixels below the threshold
+#     heatmap[heatmap < threshold] = 0
+#     heatmap[heatmap > 0] = 1
+#     # Return thresholded map
+#     return heatmap
+
+
+# def draw_labeled_bboxes(img, labels):
+#     box_list = []
+    
+#     # Iterate through all detected cars
+#     for car_number in range(1, labels[1]+1):
+#         # Find pixels with each car_number label value
+#         nonzero = (labels[0] == car_number).nonzero()
+#         # Identify x and y values of those pixels
+#         nonzeroy = np.array(nonzero[0])
+#         nonzerox = np.array(nonzero[1])
+#         # Define a bounding box based on min/max x and y
+#         bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+#         box_list.append(bbox)
+
+#     # Draw boxes on the image
+#     out_img = draw_boxes(img, box_list, color=(0, 0, 255), thick=6)
+        
+#     return out_img
